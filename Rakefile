831805c8373ad60a8ef41c82ddff7199e25625b2
Pat Allan <pat@freelancing-gods.com>
2009-10-03 22:54:21 +0200

    Restructure

--- Rakefile
+++ Rakefile
@@ -1,10 +1,4 @@
-require 'rake'
-require 'spec/rake/spectask'
+require 'tasks/distribution'
+require 'tasks/testing'
 
 task :default => :spec
-
-desc "Run all specs"
-Spec::Rake::SpecTask.new(:spec) do |t|
-  t.spec_opts = ['--options', "\"#{File.dirname(__FILE__)}/spec/spec.opts\""]
-  t.spec_files = FileList['spec/**_spec.rb']
-end
---
+++ tasks/distribution.rb
@@ -0,0 +1,23 @@
+require 'yard'
+require 'jeweler'
+
+desc 'Generate documentation'
+YARD::Rake::YardocTask.new
+
+task :rdoc => :yardoc
+
+Jeweler::Tasks.new do |gem|
+  gem.name      = "realtimebattle"
+  gem.summary   = "RealTimeBattle Ruby"
+  gem.homepage  = "http://github.com/langalex/realtimebattle_rb"
+  gem.author    = "Alex Lang, Lukas Rieder, Pat Allan"
+  gem.email     = ""
+  
+  gem.files     = FileList[
+    'lib/**/*.rb',
+    'Rakefile',
+    'tasks',
+    'VERSION.yml'
+  ]
+  gem.test_files = FileList['spec/**/*.rb']
+end
