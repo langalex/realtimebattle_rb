5c42c86da49b303b35294c19065e38d035cb230e
Alexander Lang <alex@upstream-berlin.com>
2009-10-04 18:18:56 +0200

    render wall elements

--- server/public/arena.js
+++ server/public/arena.js
@@ -1,31 +1,28 @@
 $(function(){
   function Arena(arena_selector, data){
-    var $arena = $(arena_selector);
-    $arena.canvas();
+    var arena = $(arena_selector);
+    arena.canvas();
 
     // walls
-    $.each(data.walls, function(i, wall){
-      $arena
-        .style({'strokeStyle': 'rgba( 255, 0, 0, 1)', 'fillStyle': 'rgba( 255, 0, 0, 1)', 'lineWidth': 2});
-      
-      for (var point = 0; point < wall.length-1; point++){
-        $arena
-          .beginPath()
-          .moveTo( wall[point] )
-          .lineTo( wall[point+1] )
-          .closePath()
-          .stroke();
-        
-      }
+    var walls = $.grep(data['objects'], function(obj){
+      return obj.type == 'WallElement';
     });
     
-    bots = $.grep(data['objects'], function(obj){
+    $.each(walls, function(i, wall) {
+      arena.beginPath()
+        .moveTo([wall.x, wall.y])
+        .rect([wall.x, wall.y], { 'width': 1, 'height': 1 })
+        .closePath()
+        .fill();
+    });
+    
+    var bots = $.grep(data['objects'], function(obj){
       return obj.type == 'Bot';
     });
     
     // bots
     $.each(bots, function(i, bot){
-      $arena
+      arena
         .beginPath()
         .moveTo( [bot.x, bot.y] )
         .arc(
@@ -41,14 +38,14 @@ $(function(){
         .fill();
     });
     
-    bullets = $.grep(data['objects'], function(obj){
+    var bullets = $.grep(data['objects'], function(obj){
       return obj.type == 'Bullet';
     });
     
     // bullets
     $.each(bullets, function(i, bullet){
       point0 = [bullet.x-2, bullet.y-2]; // FIXME: implement scaling of objects
-      $arena
+      arena
         .beginPath()
         .moveTo( point0 )
         .rect(
